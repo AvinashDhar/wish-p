05e0e52d5b2f2dcf833a7baa90fa8255b2c72929
Avinash Dhar <avinashdhar@Avinashs-MacBook-Air.local>
2023-08-07 08:35:02 +0530

    commiting client and server folder

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import wishLogo from './wish-logo.svg';
+import preview from './preview.png';
+
+export {
+  download,
+  logo,
+  wishLogo,
+  preview,
+};
